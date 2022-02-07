bfe4533b06eb26295aca15cceae88ab17d5c82a2
guruprasath44 <guruprasath4401@gmail.com>
2022-02-07 18:13:00 +0530

    updated calculator.java

--- src/main/java/tia/Calculator.java
+++ src/main/java/tia/Calculator.java
@@ -5,12 +5,9 @@ public class Calculator {
     public int sum(int a, int b) {
         return sumImpl(a, b);
     }
-
     private int sumImpl(int a, int b) {
-    	a =a * 5;
         return a + b;
     }
-
     public int minus(int a, int b) {
         System.out.println("guruprasath");
         return a - b;
